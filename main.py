7626bc34213fc8fba91c7d6bcea5ddef99473ad3
Blake Janes <blakeinus@gmail.com>
2021-12-05 13:23:55 -0500

    Fixed requirements.txt

--- main.py
+++ main.py
@@ -1,42 +1,25 @@
-from collections import defaultdict, Counter
-import urllib
-
-
+import PIL.Image
 import numpy
+import numpy as np
 from matplotlib import pyplot as plt
-import matplotlib.gridspec as gridspec
-from sklearn import preprocessing
-import scipy.cluster.hierarchy as sch
 
+import sys
 import nimfa
+import PIL
 
 
-def print_fit(fit):
-    def clean_axis(ax):
-        ax.get_xaxis().set_ticks([])
-        ax.get_yaxis().set_ticks([])
-        for sp in ax.spines.values():
-            sp.set_visible(False)
 
-    fig = plt.figure()
-    heatmapGS = gridspec.GridSpec(1, 2, wspace=.1, hspace=0., width_ratios=[.25, 1])
 
-    C = 1 - fit.fit.consensus()
-    Y = sch.linkage(C, method='average')
 
-    denAX = fig.add_subplot(heatmapGS[0, 0])
-    denD = sch.dendrogram(Y, orientation='right', link_color_func=lambda k: 'black')
-    clean_axis(denAX)
+line = ">QBQ33928 A/Canis lupus familiaris/USA/000915/2018 2018/01/03 PB2"
+headers = []
 
-    heatmapAX = fig.add_subplot(heatmapGS[0, 1])
-    D = C[denD['leaves'], :][:, denD['leaves']]
-    axi = heatmapAX.imshow(D, interpolation='nearest', aspect='equal', origin='lower', cmap='RdBu')
-    clean_axis(heatmapAX)
+class header:
+    def __init__(self, accession, country, date):
+        self.accession = accession
+        self.country = country
+        self.date = date
 
-    cb = fig.colorbar(axi, fraction=0.046, pad=0.04, aspect=10)
-    cb.set_label('Distance', fontsize=20)
-
-    plt.show()
 
 def read_sequences(infile: str) -> list:
     sequences = []
@@ -45,7 +28,16 @@ def read_sequences(infile: str) -> list:
         for line in inf:
             # line description  of new sequence
             if ">" in line:
-                sequences.append(list(working))
+
+                ln = line.split(" ")
+                accession = ln[0]
+                date = ln[-2]
+
+                country = "".join(ln[1:-2]).split("/")[2]
+
+                headers.append( header(accession, country, date))
+                if len(working) == 759:
+                    sequences.append(list(working))
                 working = ""
             # Line part of sequence
             else:
@@ -73,9 +65,8 @@ def build_binary(mother, sequence):
 
 
 """
-Returns Index of Ocurrence?flu  for items in a list
+Returns Index of Ocurrence? for items in a list
 """
-
 
 def ioc(li: list) -> {}:
     count = {}
@@ -86,24 +77,65 @@ def ioc(li: list) -> {}:
             count[item] = 1
     return sorted(count.items(), key=lambda x: x[1])
 
+def displayMatrix(matrix, title = "default"):
+    displayData = matrix
+
+    im = PIL.Image.fromarray(displayData)
+
+    im.show(title=title)
+
+def estimate_rank(model):
+    rank_cands = range(1,40)
+    summary = model.estimate_rank(rank_range=range(1, 40))
+    rss = [summary[rank]['rss'] for rank in rank_cands]
+    coph = [summary[rank]['cophenetic'] for rank in rank_cands]
+    disp = [summary[rank]['dispersion'] for rank in rank_cands]
+    spar = [summary[rank]['sparseness'] for rank in rank_cands]
+    spar_w, spar_h = zip(*spar)
+    evar = [summary[rank]['evar'] for rank in rank_cands]
+
+   # plt.plot(rank_cands, rss, 'o-', label='RSS', linewidth=2)
+    plt.plot(rank_cands, coph, 'o-', label='Cophenetic correlation', linewidth=2)
+    plt.plot(rank_cands, disp, 'o-', label='Dispersion', linewidth=2)
+    plt.plot(rank_cands, spar_w, 'o-', label='Sparsity (Basis)', linewidth=2)
+    plt.plot(rank_cands, spar_h, 'o-', label='Sparsity (Mixture)', linewidth=2)
+    plt.plot(rank_cands, evar, 'o-', label='Explained variance', linewidth=2)
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=3, numpoints=1)
+    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.show()
+    plt.plot(rank_cands, rss, 'o-', label='RSS', linewidth=2)
+    plt.show()
+    print(rss)
 
 seqs = read_sequences("data/FASTAP2.fa")
-
 mother = build_mother(seqs)
 
 bins = [build_binary(mother, s) for s in seqs]
-sums = [sum(bin) for bin in bins]
-print(ioc(sums))
 
+
+#Write out our matrix to a CSV for analysis later if desired
+with open("bins.csv", "w") as fout:
+    print(f"""Accession,Date,Country,{','.join([f"Position {i}" for i in range(759)])}""", file=fout)
+    for header, bin in zip(headers, bins):
+        print(f"{header.accession},{header.date},{header.country},{','.join([str(i) for i in bin])}", file=fout)
+
+
+#Print out matrix
 data = numpy.array(bins)
+displayMatrix(numpy.array([[255 * x for x in bi] for bi in bins], dtype="uint8"))
 
-bmf = nimfa.Bmf(data, n_run=20,  max_iter=20)
+
+numpy.set_printoptions(threshold=sys.maxsize)
+bmf = nimfa.Bmf(data, n_run=40,  max_iter=20, n_iter = 15, rank=9)
+
+#Estimate rank if need be
+#estimate_rank(bmf)
+bmf.rank=9
 bmf_fit = bmf()
-nmf_bins = nimfa.Nmf(data, n_run=20,  max_iter=20)
-nmf_fit = nmf_bins()
-nmf_regular = nimfa.Nmf(numpy.array([[ord(x) for x in seq] for seq in seqs]), n_run=20,  max_iter=20)
-nmf_regular_fit = nmf_regular()
 
-print_fit(bmf_fit)
-print_fit(nmf_fit)
-print_fit(nmf_regular_fit)
+np.set_printoptions(threshold=np.inf)
+print(bmf_fit.fit.W)
+with open("W.txt", "w") as fout:
+    print(bmf_fit.fit.W, file=fout)
+displayMatrix(255 - bmf_fit.fit.W * 255)
+displayMatrix(255 - bmf_fit.fit.H * 255)
